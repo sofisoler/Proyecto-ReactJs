368686f3076a3040a3ed8d9824c2de518ff848c9
Sofia Soler <sofisoler_@hotmail.com>
2023-01-23 08:16:12 -0300

    items images

--- src/utils/gFetch.js
+++ src/utils/gFetch.js
@@ -1,10 +1,13 @@
 let productos = [
-    { id: '1', category: 'makeups', name: "Labial Líquido", price: 2000, image:'https://http2.mlstatic.com/D_NQ_NP_741342-MLA40243078405_122019-O.webp' },
-    { id: '2', category: 'makeups', name: "Base Líquida", price: 5770, image: "https://http2.mlstatic.com/D_NQ_NP_852258-MLA45744618069_042021-O.webp" },
-    { id: '3', category: 'makeups', name: "Máscara De Pestañas", price: 5710, image: "https://http2.mlstatic.com/D_NQ_NP_976171-MLA49898564147_052022-O.webp" },
-    { id: '4', category: 'accessories', name: "Brochas", price: 8290, image:'https://http2.mlstatic.com/D_NQ_NP_799335-MLA43569917484_092020-O.webp' },
-    { id: '5', category: 'accessories', name: "Esponjas", price: 2890, image:'https://http2.mlstatic.com/D_NQ_NP_792553-MLA43569730984_092020-O.webp' },
-    { id: '6', category: 'accessories', name: "Arqueador", price: 2150, image:'https://http2.mlstatic.com/D_NQ_NP_923230-MLA46249431044_062021-O.webp' },
+    { id: '1', category: 'makeups', name: "Labial Líquido", price: 2000, image: '/public/items/labial-matte-ink.jpg' },
+    { id: '2', category: 'makeups', name: "Base Líquida", price: 5770, image: '/public/items/base.jpg' },
+    { id: '3', category: 'makeups', name: "Máscara De Pestañas", price: 5710, image: '/public/items/mascara-de-pestañas.jpg' },
+    { id: '4', category: 'makeups', name: "Labial Matte", price: 2500, image: '/public/items/labial-sensational.jpg' },
+    { id: '5', category: 'makeups', name: "Paleta de Sombras", price: 4800, image: '/public/items/paleta-de-sombras.jpg' },
+    { id: '6', category: 'accessories', name: "Set 5 Brochas", price: 8290, image:'/public/items/set-5-brochas.jpg' },
+    { id: '7', category: 'accessories', name: "Set 2 Esponjas", price: 2890, image:'/public/items/set-2-esponjas.jpg' },
+    { id: '8', category: 'accessories', name: "Arqueador", price: 2150, image:'/public/items/arqueador-de-pestañas.jpg' },
+    { id: '9', category: 'accessories', name: "Kit perfilador", price: 3200, image:'/public/items/kit-perfilador-de-cejas.jpg' },
 ]
 
 export const gFetch = (id)=>{
